294b497f9f5f2e24e033ce57d3d04f639594b97a
John Cornish <cornish@mymail.mines.edu>
2017-11-29 14:13:00 -0700

    Functions for setting and retrieving tags

---
+++ ztag.plugin.zsh
@@ -0,0 +1,64 @@
+ztag_setup() {
+    if [[ -f .ztagrc ]]; then
+	source .ztagrc
+    elif [[ -f ~/.ztagrc ]]; then
+	source ~/.ztagrc
+    fi
+    
+    if [ -z "${ZTAG_ROOT+1}" ]; then
+	ZTAG_ROOT=~
+    fi
+}
+
+tag() {
+    tag_name=$1
+    file=$2
+    if [ "$#" -eq 2 ]; then
+	fpath=$(readlink -f $file)
+	tag_dir=$ZTAG_ROOT/tags/$tag_name
+	mkdir -p $tag_dir
+	ln -sf $fpath $tag_dir
+    fi
+}
+
+tagsm() {
+    ztag_setup()
+    tag_name=$1
+    files=( ${@:2} )
+    for f in $files;
+    do
+	tag $tag_name $f
+    done
+}
+
+tagms() {
+    ztag_setup()
+    length=$#
+    tags=( ${@:1:$length-1} )
+    file=${@:$#}
+    for t in $tags;
+    do
+	tag $t $file
+    done
+}
+
+tagls() {
+    tags=( $@ )
+    links=()
+    for t in $tags;
+    do
+	this_tag=($(find $ZTAG_ROOT/tags/$t -type l))
+	links=($links $this_tag)
+    done
+
+    files=()
+    for l in $links;
+    do
+	this_link=$(readlink -f $l)
+	files+=$this_link
+    done
+
+    echo "${(@u)files}"
+}
+
+ztag_setup
